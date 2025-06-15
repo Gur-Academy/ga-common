8d4ae3a5960438e82de67117c188bb5b72995aae
codebyjsa <jsaishere@gmail.com>
2025-06-15 12:12:37 +0530

    created project

---
+++ src/main/java/com/guracademy/common/GaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.guracademy.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaCommonApplication.class, args);
+	}
+
+}
